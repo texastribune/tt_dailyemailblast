95b5808bc8e56deb1d4e298b142518cd0c48694c
Travis Swicegood <development@domain51.com>
2013-04-09 16:13:33 -0500

    Sketch out some more non-async code

--- tt_dailyemailblast/models.py
+++ tt_dailyemailblast/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.template import Template
+from django.util.text import slugify
+
+from . import email
 
 
-class Receipient(models.Model):
+class NamedSlugMixin(object):
+    @property
+    def slug(self):
+        return slugify(self.name)
+
+
+class Receipient(NamedSlugMixin, models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     active = models.BooleanField(default=True)
@@ -9,15 +19,46 @@ class Receipient(models.Model):
     def __unicode__(self):
         return u'%s (%s)' % (self.name, self.email)
 
+    def send(self, receipient_list, blast):
+        email.send_email(blast.render(self, receipient_list))
 
-class ReceipientList(models.Model):
+
+class ReceipientList(NamedSlugMixin, models.Model):
     name = models.CharField(max_length=255)
     receipients = models.ManyToManyField(Receipient, related_name='lists')
 
     def __unicode__(self):
         return u'%s (%s)' % (self.name, self.receipients.count())
 
+    def send(self, blast):
+        for r in self.receipients.all():
+            r.send(blast)
 
-class DailyEmailBlast(models.Model):
+
+class DailyEmailBlast(NamedSlugMixin, models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
+    receipient_lists = models.ManyToManyField(ReceipientList,
+            related_name='blasts')
+
+    def send(self):
+        for l in self.receipient_lists.all():
+            l.send(self)
+
+    def render(self, receipient, receipient_list):
+        context = {
+            'receipient': receipient,
+            'receipient_list': receipient_list,
+            'blast': self,
+        }
+        t = Template(get_template_names(self, receipient_list, receipient))
+        return t.render(context)
+
+
+def get_template_names(blast, receipient_list, receipient):
+    return [
+        'tt_dailyemailblast/%s/%s/%s.html' % (blast.slug, receipient_list.slug,
+                receipient.slug),
+        'tt_dailyemailblast/%s/%s.html' % (blast.slug, receipient_list.slug),
+        'tt_dailyemailblast/%s.html' % blast.slug,
+    ]
