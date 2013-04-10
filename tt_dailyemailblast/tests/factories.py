2125c171cf441e2ec50c50517f7b4285fb6ea60d
Travis Swicegood <development@domain51.com>
2013-04-10 12:51:01 -0500

    Doh, these were supposed to be Sequences (oops)

--- tt_dailyemailblast/tests/factories.py
+++ tt_dailyemailblast/tests/factories.py
@@ -5,24 +5,24 @@ from .. import models
 
 class ReceipientFactory(factory.DjangoModelFactory):
     FACTORY_FOR = models.Receipient
-    name = factory.LazyAttribute(lambda n: 'Alice Example {0}'.format(n))
-    email = factory.LazyAttribute(lambda n: 'alice{0}@example.com'.format(n))
+    name = factory.Sequence(lambda n: 'Alice Example {0}'.format(n))
+    email = factory.Sequence(lambda n: 'alice{0}@example.com'.format(n))
 
 
 class ReceipientListFactory(factory.DjangoModelFactory):
     FACTORY_FOR = models.ReceipientList
-    name = factory.LazyAttribute(
+    name = factory.Sequence(
             lambda n: 'Random Receipient List {0}'.format(n))
     receipients = factory.RelatedFactory(ReceipientFactory)
 
 
 class DailyEmailBlastTypeFactory(factory.DjangoModelFactory):
     FACTORY_FOR = models.DailyEmailBlastType
-    name = factory.LazyAttribute(lambda n: 'Blast Type {0}'.format(n))
+    name = factory.Sequence(lambda n: 'Blast Type {0}'.format(n))
 
 
 class DailyEmailBlastFactory(factory.DjangoModelFactory):
     FACTORY_FOR = models.DailyEmailBlast
-    body = factory.LazyAttribute(lambda n: 'Some random body {0}'.format(n))
+    body = factory.Sequence(lambda n: 'Some random body {0}'.format(n))
     receipient_lists = factory.RelatedFactory(ReceipientListFactory)
     blast_type = factory.SubFactory(DailyEmailBlastTypeFactory)
