928c3c32015b9704f5d3a8b67b29ee35306ce685
Travis Swicegood <development@domain51.com>
2013-04-16 11:46:46 -0500

    Fix spelling error

--- tt_dailyemailblast/send_backends.py
+++ tt_dailyemailblast/send_backends.py
@@ -1,12 +1,12 @@
 def sync_dailyemailblasts(blast):
-    for l in blast.receipient_lists.all():
+    for l in blast.recipients_lists.all():
         l.send(blast)
 
 
-def sync_receipientlist(receipient_list, blast):
-    for r in receipient_list.receipients.all():
-        r.send(receipient_list, blast)
+def sync_recipientslist(recipients_list, blast):
+    for r in recipients_list.recipientss.all():
+        r.send(recipients_list, blast)
 
 
-def sync_receipient(receipient, receipient_list, blast):
-    email.send_email(blast.render(receipient, receipient_list))
+def sync_recipients(recipients, recipients_list, blast):
+    email.send_email(blast.render(recipients, recipients_list))
