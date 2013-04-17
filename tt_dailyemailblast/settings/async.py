9746d2082643ce7d75b0e8374f0b21b7a9965c46
Travis Swicegood <development@domain51.com>
2013-04-17 16:23:32 -0500

    Add in modules that can be imported for the sync/async settings

---
+++ tt_dailyemailblast/settings/async.py
@@ -0,0 +1,11 @@
+TT_DAILYEMAILBLAST_RECIPIENT = [
+    'tt_dailyemailblast.send_backends.async.recipient',
+]
+
+TT_DAILYEMAILBLAST_RECEIPIENTLIST = [
+    'tt_dailyemailblast.send_backends.async.recipient_list',
+]
+
+TT_DAILYEMAILBLAST_BLASTBACKEND = [
+    'tt_dailyemailblast.send_backends.async.daily_email_blasts',
+]
---
+++ tt_dailyemailblast/settings/sync.py
@@ -0,0 +1,11 @@
+TT_DAILYEMAILBLAST_RECIPIENT = [
+    'tt_dailyemailblast.send_backends.sync.recipient',
+]
+
+TT_DAILYEMAILBLAST_RECEIPIENTLIST = [
+    'tt_dailyemailblast.send_backends.sync.recipient_list',
+]
+
+TT_DAILYEMAILBLAST_BLASTBACKEND = [
+    'tt_dailyemailblast.send_backends.sync.daily_email_blasts',
+]
