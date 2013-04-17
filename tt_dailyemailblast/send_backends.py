f1ca706ef49932bff6f462f08be63487f40858f2
Travis Swicegood <development@domain51.com>
2013-04-17 15:34:58 -0500

    Add untested async tasks

--- tt_dailyemailblast/send_backends.py
+++ tt_dailyemailblast/send_backends.py
@@ -1,3 +1,6 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import ugettext_lazy as _
+
 from . import email
 
 
@@ -6,10 +9,38 @@ def sync_daily_email_blasts(blast):
         l.send(blast)
 
 
+def async_daily_email_blasts(blast):
+    try:
+        from .tasks import send_daily_email_blasts
+    except ImportError, e:
+        msg = _(u"Please install Celery before using the async backend: %s" % e)
+        raise ImproperlyConfigured(msg)
+    send_daily_email_blasts.apply_async(args=[blast.pk])
+
+
 def sync_recipients_list(recipients_list, blast):
     for r in recipients_list.recipientss.all():
         r.send(recipients_list, blast)
 
 
+def async_recipients_list(recipients_list, blast):
+    try:
+        from .tasks import send_recipients_list
+    except ImportError, e:
+        msg = _(u"Please install Celery before using the async backend: %s" % e)
+        raise ImproperlyConfigured(msg)
+    send_recipients_list.apply_async(args=[recipients_list.pk, blast.pk])
+
+
 def sync_recipients(recipients, recipients_list, blast):
     email.send_email(blast.render(recipients, recipients_list))
+
+
+def async_recipients(recipients, recipients_list, blast):
+    try:
+        from .tasks import send_recipients
+    except ImportError, e:
+        msg = _(u"Please install Celery before using the async backend: %s" % e)
+        raise ImproperlyConfigured(msg)
+    send_recipients.apply_async(args=[recipients.pk, recipients_list.pk,
+            blast.pk])
---
+++ tt_dailyemailblast/tasks.py
@@ -0,0 +1,25 @@
+from celery.task import task
+
+from . import models
+from . import send_backends
+
+
+@task
+def send_daily_email_blasts(blast_pk):
+    blast = models.DailyEmailBlast.objects.get(pk=blast_pk)
+    send_backends.sync_daily_email_blasts(blast)
+
+
+@task
+def send_recipients_list(recipients_list_pk, blast_pk):
+    blast = models.DailyEmailBlast.objects.get(pk=blast_pk)
+    recipients_list = models.ReceipientList.objects.get(pk=recipients_list_pk)
+    send_backends.sync_recipients_list(recipients_list, blast)
+
+
+@task
+def send_recipients(recipient_pk, recipients_list_pk, blast_pk):
+    blast = models.DailyEmailBlast.objects.get(pk=blast_pk)
+    recipients_list = models.ReceipientList.objects.get(pk=recipients_list_pk)
+    recipient = models.Receipient.objects.get(pk=recipient_pk)
+    send_backends.sync_recipients(recipient, recipients_list, blast)
