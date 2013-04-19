98fad1af84abe13eb64baad58c8a2faf3cd6cccb
Noah Seger <noah@texastribune.org>
2013-04-19 18:00:06 -0500

    Fix every single async task was broken

--- tt_dailyemailblast/tasks.py
+++ tt_dailyemailblast/tasks.py
@@ -1,20 +1,20 @@
 from celery.task import task
 
 from . import models
-from . import send_backends
+from .send_backends import sync
 
 
 @task
 def send_daily_email_blasts(blast_pk):
     blast = models.DailyEmailBlast.objects.get(pk=blast_pk)
-    send_backends.sync_daily_email_blasts(blast)
+    sync.sync_daily_email_blasts(blast)
 
 
 @task
 def send_recipients_list(recipients_list_pk, blast_pk):
     blast = models.DailyEmailBlast.objects.get(pk=blast_pk)
     recipients_list = models.RecipientList.objects.get(pk=recipients_list_pk)
-    send_backends.sync_recipients_list(recipients_list, blast)
+    sync.sync_recipients_list(recipients_list, blast)
 
 
 @task
@@ -22,4 +22,4 @@ def send_recipients(recipient_pk, recipients_list_pk, blast_pk):
     blast = models.DailyEmailBlast.objects.get(pk=blast_pk)
     recipients_list = models.RecipientList.objects.get(pk=recipients_list_pk)
     recipient = models.Recipient.objects.get(pk=recipient_pk)
-    send_backends.sync_recipient(recipient, recipients_list, blast)
+    sync.sync_recipient(recipient, recipients_list, blast)
