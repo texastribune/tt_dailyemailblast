3121f15ffc67bef158cfe48a9997833c1f15a611
Noah Seger <noah@texastribune.org>
2013-04-19 17:34:50 -0500

    Add some admin with inlines

---
+++ tt_dailyemailblast/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+
+from .models import (Recipient, RecipientList, DailyEmailBlast,
+        DailyEmailBlastType)
+
+
+class RecipientInline(admin.TabularInline):
+    model = RecipientList.recipients.through
+    verbose_name = 'recipient'
+    verbose_name_plural = 'recipients'
+
+
+class RecipientListAdmin(admin.ModelAdmin):
+    model = RecipientList
+    inlines = [RecipientInline]
+
+
+class RecipientListInline(admin.TabularInline):
+    model = DailyEmailBlast.recipient_lists.through
+    verbose_name = 'recipient list'
+    verbose_name_plural = 'recipient lists'
+
+
+class DailyEmailBlastAdmin(admin.ModelAdmin):
+    model = DailyEmailBlast
+    inlines = [RecipientListInline]
+
+
+admin.site.register(DailyEmailBlastType)
+admin.site.register(Recipient)
+admin.site.register(RecipientList, RecipientListAdmin)
+admin.site.register(DailyEmailBlast, DailyEmailBlastAdmin)
