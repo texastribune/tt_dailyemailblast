1dabd42e01093a4ecfdc962a1d12746ef60a5aab
Travis Swicegood <development@domain51.com>
2013-04-24 15:27:34 -0500

    Bump to next dev version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ for dirpath, dirnames, filenames in os.walk('tt_dailyemailblast'):
 
 setup(
     name='tt_dailyemailblast',
-    version='0.1.0',
+    version='0.2alpha.0',
     description='Texas Tribune: tt_dailyemailblast',
     author='Tribune Tech',
     author_email='tech@texastribune.org',
