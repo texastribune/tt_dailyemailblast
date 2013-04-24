e63626054ce9286e661c7a713f32c329dedf5a20
Travis Swicegood <development@domain51.com>
2013-04-24 17:03:22 -0500

    Simply use the blast subject

--- tt_dailyemailblast/send_backends/sync.py
+++ tt_dailyemailblast/send_backends/sync.py
@@ -1,6 +1,5 @@
 import logging
 
-from armstrong.utils.backends import GenericBackend
 from django.conf import settings
 
 from .. import email
@@ -22,10 +21,7 @@ def sync_recipient_list(recipients_list, blast):
 
 def sync_recipient(recipient, recipients_list, blast):
     html = blast.render(recipient, recipients_list)
-    context_backend = GenericBackend('TT_DAILYEMAILBLAST_CONTEXT',
-            defaults=['tt_dailyemailblast.context_backends.basic'])
-    context = context_backend.get_backend(blast, recipient, recipients_list)
-    subject = blast.blast_type.name.format(**context)
+    subject = blast.subject
     bodies = {'html': html}  # TODO: Support plain text rendering
     to = (recipient.email,)
     from_email = settings.TT_DAILYEMAILBLAST_FROMEMAIL
