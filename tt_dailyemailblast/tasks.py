8a0d828508ec0b6ba2d8add7f61b0e33669a893d
Daniel Craigmile <dcraigmile@texastribune.org>
2013-12-20 11:34:24 -0600

    fix logging level

--- tt_dailyemailblast/tasks.py
+++ tt_dailyemailblast/tasks.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 @task
 def send_daily_email_blasts(blast_pk):
     blast = models.DailyEmailBlast.objects.get(pk=blast_pk)
-    logger.error('Sending blast for %s' % blast)
+    logger.debug('Sending blast for %s' % blast)
     sync.sync_daily_email_blasts(blast)
 
 
@@ -19,7 +19,7 @@ def send_daily_email_blasts(blast_pk):
 def send_recipients_list(recipients_list_pk, blast_pk):
     blast = models.DailyEmailBlast.objects.get(pk=blast_pk)
     recipients_list = models.RecipientList.objects.get(pk=recipients_list_pk)
-    logger.error('Sending blast for %s to %s' % (blast, recipients_list))
+    logger.debug('Sending blast for %s to %s' % (blast, recipients_list))
     sync.sync_recipient_list(recipients_list, blast)
 
 
@@ -28,6 +28,6 @@ def send_recipient(recipient_pk, recipients_list_pk, blast_pk):
     blast = models.DailyEmailBlast.objects.get(pk=blast_pk)
     recipients_list = models.RecipientList.objects.get(pk=recipients_list_pk)
     recipient = models.Recipient.objects.get(pk=recipient_pk)
-    logger.error('Sending blast %s to %s of list %s' % (blast, recipient,
+    logger.debug('Sending blast %s to %s of list %s' % (blast, recipient,
             recipients_list))
     sync.sync_recipient(recipient, recipients_list, blast)
